ef42876a3e4d375856ea6466f18cd5e917f4c02b
Akanimo47 <akanebong0147@gmail.com>
2023-10-05 17:34:31 +0100

    updated once

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -1,14 +1,15 @@
-public class job
+using System;
+
+public class Job
 {
-    public string _company = " ";
-    public string _jobTitle = " ";
+ public string _companyName;
+ public string _jobTitle;
+ public int _startYear;
+ public int _endYear;
 
-    public int _startYear = 0;
-    public int _endYear = 0;
+ public void DisplayJobDetails() 
+ {
+    Console.WriteLine($"{_jobTitle} ({_companyName}) {_startYear}-{_endYear}");
+ }
 
-    public void Display()
-    {
-        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
-    }
-       
 }
